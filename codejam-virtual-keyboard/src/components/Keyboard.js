ae1a03c1c905c9560c527b91e084bcc5afc38972
spomni <mikhail.snitko@gmail.com>
2019-11-06 00:19:30 +0300

    refactor: Fix code style.

--- codejam-virtual-keyboard/src/components/Keyboard.js
+++ codejam-virtual-keyboard/src/components/Keyboard.js
@@ -1,7 +1,7 @@
 require('./Keyboard.scss');
 
-const createElement = require('../js/lib/create-element');
 const elementResizeEvent = require('element-resize-event');
+const createElement = require('../js/lib/create-element');
 const layouts = require('./keyboard-layouts');
 
 const symbolKeyCodes = [
@@ -54,7 +54,7 @@ const symbolKeyCodes = [
   'Slash',
 ];
 
-/** Check if passed key node is key of symbol to typing. 
+/** Check if passed key node is key of symbol to typing.
  *
  * @param {HTMLElement} key
  */
@@ -63,42 +63,42 @@ const isSymbolKey = (key) => symbolKeyCodes.includes(key.dataset.code);
 /** Generate DOM structure for Keyboard component and insert it into the "node" element.
  * @param {HTMLElement} node - Keyboard component mount point.
 */
-function createKeyboardDOM(node) {
-  if (!node.classList.contains('keyboard')) { 
+const createKeyboardDOM = (node) => {
+  if (!node.classList.contains('keyboard')) {
     node.classList.add('keyboard');
   }
 
   layouts.codes.forEach((lineStr) => {
     const line = createElement('div', 'keyboard__line');
     node.appendChild(line);
-    
-    lineStr.split(' ').forEach((keyCode => {
-      const key = createElement('div', 'keyboard__key', { 
-        'data-code': keyCode
+
+    lineStr.split(' ').forEach(((keyCode) => {
+      const key = createElement('div', 'keyboard__key', {
+        'data-code': keyCode,
       });
-      
+
       line.appendChild(key);
     }));
-  })
-}
+  });
+};
 
 /** Calculate and set sizes of the DOM elements
  * @param {HTMLElement} node - Keyboard component mount point.
  */
-function setSizes(node) {
+const setSizes = (node) => {
   const keyboardWidth = parseInt(getComputedStyle(node).width, 10);
   const keyHeight = keyboardWidth * 0.05;
   const keyGap = (keyboardWidth - keyHeight * 16) / 17;
-  
+
   const firstLine = node.firstChild;
   firstLine.style.paddingTop = `${keyGap}px`;
 
-  const keys = Array.from(node.querySelectorAll('.keyboard__key'));
+  const keys = [...node.querySelectorAll('.keyboard__key')];
 
   keys.forEach((currKey) => {
     const { code } = currKey.dataset;
     const keyStyle = currKey.style;
-  
+
     keyStyle.width = `${keyHeight}px`;
     keyStyle.height = `${keyHeight}px`;
     keyStyle.margin = `0 0 ${keyGap}px ${keyGap}px`;
@@ -106,111 +106,110 @@ function setSizes(node) {
     keyStyle.fontSize = isSymbolKey(currKey)
       ? `${keyHeight / 2}px`
       : `${keyHeight / 3.5}px`;
-    
+
     switch (code) {
       case 'F1':
         keyStyle.marginLeft = `${keyGap * 2 + keyHeight}px`;
         break;
-    
+
       case 'F5':
       case 'F9':
         keyStyle.marginLeft = `${(keyGap * 3 + keyHeight) / 2}px`;
         break;
-    
+
       case 'Backspace':
         keyStyle.width = `${keyHeight * 2 + keyGap}px`;
         break;
-    
+
       case 'Tab':
         keyStyle.width = `${keyHeight * (1 + 1 / 3) + keyGap}px`;
         break;
-    
+
       case 'Backslash':
         keyStyle.width = `${keyHeight * (1 + 2 / 3)}px`;
         break;
-    
+
       case 'CapsLock':
         keyStyle.width = `${keyHeight * (1 + 2 / 3) + keyGap}px`;
         break;
-    
+
       case 'Enter':
         keyStyle.width = `${keyHeight * (2 + 1 / 3) + keyGap}px`;
         break;
-    
+
       case 'ShiftLeft':
         keyStyle.width = `${keyHeight * (1 + 4 / 3) + keyGap}px`;
         break;
-    
+
       case 'ShiftRight':
         keyStyle.width = `${keyHeight * (1 + 2 / 3) + keyGap}px`;
         break;
-    
+
       case 'ControlLeft':
         keyStyle.width = `${keyHeight * (1 + 2 / 3)}px`;
         break;
-    
+
       case 'Space':
         keyStyle.width = `${keyHeight * 6 + keyGap * 5}px`;
         break;
-    
+
       case 'ControlRight':
         keyStyle.width = `${keyHeight * (1 + 1 / 3) + keyGap}px`;
         break;
-    
+
       default:
         break;
     }
   });
-}
+};
 
 /* Set keyboard layout and print its symbols on the keys.
  *
  * @param {object} - the keyboard instance state
  * @param {HTMLElement} state.node - Keyboard component mount point.
- * @param {oolean} isShifted - Contains true if should use the layout for the case when Shift key is pressed.
+ * @param {boolean} isShiftPressed - What layout should use.
  */
-const setLayout = (state) => {
-
-  const {node, isShifted, layoutName} = state;
-
+const setLayout = ({ node, isShiftPressed, layoutName }) => {
   if (!layouts[layoutName]) {
-    throw new Error(`Incorrect layout name "${layoutName}."`)
+    throw new Error(`Incorrect layout name "${layoutName}."`);
   }
-  
-  const newLayout = isShifted 
+
+  const newLayout = isShiftPressed
     ? layouts[layoutName][1]
     : layouts[layoutName][0];
-   
+
   layouts.codes.forEach((lineStr, lineNumber) => {
     lineStr.split(' ').forEach((code, keyNumber) => {
       const key = node.querySelector(`[data-code=${code}]`);
       const value = newLayout[lineNumber].split(' ')[keyNumber];
-      
+
       key.innerHTML = value;
       if (isSymbolKey(key)) key.setAttribute('data-symbol', value);
     });
   });
-}
+};
 
-function Keyboard(opts) {
-  const {node} = opts;
-  
+/** Create the Keyboard component and attach it to the "node" option.
+ * @param {object} opts
+ * @param {HTMLElement} opts.node - App mount point
+ */
+function Keyboard({ node }) {
   createKeyboardDOM(node);
   setSizes(node);
-  
-  let state = {
+
+  const state = {
     node,
-    isShifted: false,
-    layoutName: 'en-us'
+    isShiftPressed: false,
+    layoutName: 'en-us',
   };
 
   setLayout(state);
-  
+
   /* Dzmitry, I remember your desire to write like this:
   *    elementResizeEvent(node, setSizes.bind(null, node));
   *
-  *  But I think the way with the arrow function is more 
-  *    readable in this case.
+  *  But I think the way with the arrow function is more
+  *    readable in this certain case.
   */
   elementResizeEvent(node, () => setSizes(node));
 }
