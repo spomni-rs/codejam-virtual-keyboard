dfe47daadd31180d883a3c5fa89315ebd0c07cee
spomni <mikhail.snitko@gmail.com>
2019-11-05 18:32:28 +0300

    feat: Generate keyboard DOM and style it.

--- codejam-virtual-keyboard/src/components/Keyboard.js
+++ codejam-virtual-keyboard/src/components/Keyboard.js
@@ -1,5 +1,120 @@
-function Keyboard() {
-  console.log('This is a mock of the Keyboard component');
+require('./Keyboard.scss');
+
+const createElement = require('../js/lib/create-element');
+const elementResizeEvent = require('element-resize-event');
+const layouts = require('./keyboard-layouts');
+
+/** Generate DOM structure for Keyboard component and insert it into the "node" element.
+ * @param {HTMLElement} node - Keyboard component mount point.
+*/
+function createKeyboardDOM(node) {
+  if (!node.classList.contains('keyboard')) { 
+    node.classList.add('keyboard');
+  }
+
+  layouts.codes.forEach((lineStr) => {
+    const line = createElement('div', 'keyboard__line');
+    node.appendChild(line);
+    
+    lineStr.split(' ').forEach((keyCode => {
+      const key = createElement('div', 'keyboard__key', { 
+        'data-code': keyCode
+      });
+      
+      line.appendChild(key);
+    }));
+  })
+}
+
+/** Calculate and set sizes of the DOM elements
+ * @param {HTMLElement} node - Keyboard component mount point.
+ */
+function setSizes(node) {
+  const keyboardWidth = parseInt(getComputedStyle(node).width, 10);
+  const keyHeight = keyboardWidth * 0.05;
+  const keyGap = (keyboardWidth - keyHeight * 16) / 17;
+  
+  const firstLine = node.firstChild;
+  firstLine.style.paddingTop = `${keyGap}px`;
+
+  const keys = Array.from(node.querySelectorAll('.keyboard__key'));
+
+  keys.forEach((currKey) => {
+    const { code } = currKey.dataset;
+    const keyStyle = currKey.style;
+  
+    keyStyle.width = `${keyHeight}px`;
+    keyStyle.height = `${keyHeight}px`;
+    keyStyle.margin = `0 0 ${keyGap}px ${keyGap}px`;
+    keyStyle.fontSize = `${keyHeight / 3.5}px`;
+    
+    switch (code) {
+      case 'F1':
+        keyStyle.marginLeft = `${keyGap * 2 + keyHeight}px`;
+        break;
+    
+      case 'F5':
+      case 'F9':
+        keyStyle.marginLeft = `${(keyGap * 3 + keyHeight) / 2}px`;
+        break;
+    
+      case 'Backspace':
+        keyStyle.width = `${keyHeight * 2 + keyGap}px`;
+        break;
+    
+      case 'Tab':
+        keyStyle.width = `${keyHeight * (1 + 1 / 3) + keyGap}px`;
+        break;
+    
+      case 'Backslash':
+        keyStyle.width = `${keyHeight * (1 + 2 / 3)}px`;
+        break;
+    
+      case 'CapsLock':
+        keyStyle.width = `${keyHeight * (1 + 2 / 3) + keyGap}px`;
+        break;
+    
+      case 'Enter':
+        keyStyle.width = `${keyHeight * (2 + 1 / 3) + keyGap}px`;
+        break;
+    
+      case 'ShiftLeft':
+        keyStyle.width = `${keyHeight * (1 + 4 / 3) + keyGap}px`;
+        break;
+    
+      case 'ShiftRight':
+        keyStyle.width = `${keyHeight * (1 + 2 / 3) + keyGap}px`;
+        break;
+    
+      case 'ControlLeft':
+        keyStyle.width = `${keyHeight * (1 + 2 / 3)}px`;
+        break;
+    
+      case 'Space':
+        keyStyle.width = `${keyHeight * 6 + keyGap * 5}px`;
+        break;
+    
+      case 'ControlRight':
+        keyStyle.width = `${keyHeight * (1 + 1 / 3) + keyGap}px`;
+        break;
+    
+      default:
+        break;
+    }
+  });
+}
+ 
+function Keyboard(opts) {
+  const {node} = opts;
+  
+  createKeyboardDOM(node);
+  setSizes(node);
+  
+  /* Dzmitry, I remember your desire to write like this:
+  *    elementResizeEvent(node, setSizes.bind(null, node));
+  *  But I think the way with the arrow function is more readable.
+  */
+  elementResizeEvent(node, () => setSizes(node));
 }
 
 module.exports = Keyboard;
